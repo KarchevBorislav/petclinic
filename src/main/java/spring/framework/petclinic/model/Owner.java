527c8d43fe85a86351f32ce5da57f1c93a640968
KarchevBorislav <bkarchev@web.de>
2024-09-23 21:00:03 +0200

    added visits.

--- src/main/java/spring/framework/petclinic/model/Owner.java
+++ src/main/java/spring/framework/petclinic/model/Owner.java
@@ -1,5 +1,14 @@
 package spring.framework.petclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Setter
+@Getter
 public class Owner extends Person {
+
+    private Set<Pet> pets;
 
 }
---
+++ src/main/java/spring/framework/petclinic/model/Visit.java
@@ -0,0 +1,17 @@
+package spring.framework.petclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+    private  String description;
+    private Pet pet;
+
+        
+}
