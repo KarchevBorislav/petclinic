8e7eb65d38d37b33b47f1262fe2ce0b08da61ba8
KarchevBorislav <bkarchev@web.de>
2024-09-25 20:48:11 +0200

    Implemented Base Entity.

--- src/main/java/spring/framework/petclinic/model/BaseEntity.java
+++ src/main/java/spring/framework/petclinic/model/BaseEntity.java
@@ -1,5 +1,9 @@
 package spring.framework.petclinic.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +11,12 @@ import java.io.Serializable;
 
 @Setter
 @Getter
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
 }
