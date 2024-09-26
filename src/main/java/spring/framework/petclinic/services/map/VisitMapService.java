0cf54bcb93cc53163b1a8caf0d17789057a1eb81
KarchevBorislav <bkarchev@web.de>
2024-09-26 13:07:55 +0200

    Update VisitMapService.java

--- src/main/java/spring/framework/petclinic/services/map/VisitMapService.java
+++ src/main/java/spring/framework/petclinic/services/map/VisitMapService.java
@@ -28,7 +28,7 @@ public class VisitMapService extends AbstractMapService<Visit, Integer> implemen
     @Override
     public Visit save(Visit visit) {
         if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null || visit.getPet().getOwner().getId()== null)  {
-throw new RuntimeException("Invalid visit")
+throw new RuntimeException("Invalid visit");
 
         }
         return super.save(visit);
