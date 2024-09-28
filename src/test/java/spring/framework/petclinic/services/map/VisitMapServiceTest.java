6f6ac432a99232e5223bea950142b21b9a279df4
KarchevBorislav <bkarchev@web.de>
2024-09-28 11:32:33 +0200

    Adding Tests for the rest MapServices

---
+++ src/test/java/spring/framework/petclinic/services/map/VisitMapServiceTest.java
@@ -0,0 +1,33 @@
+package spring.framework.petclinic.services.map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VisitMapServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
