47338d3e216fe5f05f673434835614e1d8947c40
KarchevBorislav <bkarchev@web.de>
2024-09-28 09:45:09 +0200

    Upgrade to Junit 5.Close #4

--- src/test/java/spring/framework/petclinic/PetClinicApplicationTest.java
+++ src/test/java/spring/framework/petclinic/PetClinicApplicationTest.java
@@ -1,5 +1,6 @@
 package spring.framework.petclinic;
 
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,5 +10,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class PetClinicApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
 
 }
